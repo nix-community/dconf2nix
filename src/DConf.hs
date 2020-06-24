b60ae433e33f1b3fffc6028cce44cae3f80b853e
gvolpe <volpegabriel@gmail.com>
2020-06-24 23:50:56 +0200

    partially parsing lists

--- src/DConf.hs
+++ src/DConf.hs
@@ -5,6 +5,7 @@ module DConf where
 import Data.Char (toLower)
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.List (intersperse)
 import System.IO (appendFile, readFile, writeFile)
 
 type Field = String
@@ -25,33 +26,53 @@ data Entry = Entry
 entryToNix :: Entry -> Nix
 entryToNix (Entry h c) =
   let header = "  \"" ++ h ++ "\" = {\n"
-      body   = (Map.toList c) >>= (\(f, v) -> "    \"" ++ f ++ "\"=" ++ parseValue v ++ "\n")
+      body   = (Map.toList c) >>= (\(f, v) -> "    \"" ++ f ++ "\" = " ++ valueToNix v ++ "\n")
       close  = "  };\n\n"
   in header ++ body ++ close
 
-parseValue :: Value -> String
-parseValue (S v) = "\"" ++ trim (takeWhile (/= '\'') (drop 1 v)) ++ "\";"
-parseValue (B v) = (toLower <$> show v) ++ ";"
-parseValue (D v) = show v ++ ";"
-parseValue _     = ""
+valueToNix :: Value -> Nix
+valueToNix raw = valueToNix' raw ++ ";"
+ where
+  valueToNix' (S v)  = "\"" ++ trim (takeWhile (/= '\'') (drop 1 v)) ++ "\""
+  valueToNix' (B v)  = (toLower <$> show v)
+  valueToNix' (I v)  = show v
+  valueToNix' (D v)  = show v
+  valueToNix' (L xs) =
+    let ls = concat ((\c -> c ++ " ") <$> (valueToNix' <$> xs))
+    in  "[ " ++ ls ++ "]"
 
 ---------- DConf to Entry -----------
 
+-- TODO: use parser combinators
 parseContent :: [String] -> Content
-parseContent []   = Map.fromList []
+parseContent [] = Map.fromList []
 parseContent xs = Map.fromList (xs >>= f)
  where
   f [] = []
   f ys =
     let field = trim $ takeWhile (/= '=') ys
         raw   = trim $ drop (length field + 1) ys
-        value = case raw of
-          "false" -> B False
-          "true"  -> B True
-          v       -> case (readNumber v :: Maybe Double) of
-            Nothing -> S v
-            Just n  -> D n
+        value = case parseList raw of
+          Nothing -> parseValue raw
+          Just l  -> l
     in  [(field, value)]
+
+parseValue :: String -> Value
+parseValue raw = case raw of
+  "false" -> B False
+  "true"  -> B True
+  v       -> numberOr v
+ where
+  numberOr :: String -> Value
+  numberOr v = case (readNumber v :: Maybe Int) of
+    Nothing -> case (readNumber v :: Maybe Double) of
+      Nothing -> S v
+      Just n  -> D n
+    Just n  -> I n
+
+parseList :: String -> Maybe Value
+parseList ('[':xs) = Just (L $ parseValue <$> words (takeWhile (/= ']') xs))
+parseList _        = Nothing
 
 readNumber :: forall a . (Num a, Read a) => String -> Maybe a
 readNumber s =
