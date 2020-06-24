3c2a91b9a0e21bd58d3a4880af22229210ceac9a
gvolpe <volpegabriel@gmail.com>
2020-06-24 23:00:11 +0200

    parsing String, Bool and Double

--- src/DConf.hs
+++ src/DConf.hs
@@ -1,11 +1,16 @@
+{-# LANGUAGE ScopedTypeVariables, RankNTypes #-}
+
 module DConf where
 
+import Data.Char (toLower)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import System.IO (appendFile, readFile, writeFile)
 
 type Field = String
 data Value = S String | B Bool | I Int | D Double | L [Value] deriving Show
+
+type Nix = String
 
 type Header  = String
 type Content = Map Field Value
@@ -15,14 +20,22 @@ data Entry = Entry
   , content :: Content
   } deriving Show
 
-type Nix = String
+---------- Entry to Nix -----------
 
 entryToNix :: Entry -> Nix
 entryToNix (Entry h c) =
   let header = "  \"" ++ h ++ "\" = {\n"
-      body   = (Map.toList c) >>= (\(f, (S v)) -> "    \"" ++ f ++ "\"=" ++ v ++ "\n")
-      close  = "  }\n"
+      body   = (Map.toList c) >>= (\(f, v) -> "    \"" ++ f ++ "\"=" ++ parseValue v ++ "\n")
+      close  = "  };\n\n"
   in header ++ body ++ close
+
+parseValue :: Value -> String
+parseValue (S v) = "\"" ++ trim (takeWhile (/= '\'') (drop 1 v)) ++ "\";"
+parseValue (B v) = (toLower <$> show v) ++ ";"
+parseValue (D v) = show v ++ ";"
+parseValue _     = ""
+
+---------- DConf to Entry -----------
 
 parseContent :: [String] -> Content
 parseContent []   = Map.fromList []
@@ -31,22 +44,31 @@ parseContent xs = Map.fromList (xs >>= f)
   f [] = []
   f ys =
     let field = trim $ takeWhile (/= '=') ys
-        value = S . trim $ drop (length field + 1) ys
+        raw   = trim $ drop (length field + 1) ys
+        value = case raw of
+          "false" -> B False
+          "true"  -> B True
+          v       -> case (readNumber v :: Maybe Double) of
+            Nothing -> S v
+            Just n  -> D n
     in  [(field, value)]
+
+readNumber :: forall a . (Num a, Read a) => String -> Maybe a
+readNumber s =
+  case (reads s :: [(a, String)]) of
+    (i, _):_ -> Just i
+    []       -> Nothing
 
 parseEntry :: [String] -> Maybe Entry
 parseEntry []      = Nothing
 parseEntry (h : t) = Just (Entry (parseHeader h) (parseContent t))
 
-trim :: String -> String
-trim xs =
-  let trim' ys = dropWhile (== ' ') ys
-  in  trim' (reverse $ trim' (reverse xs))
-
 parseHeader :: String -> String
 parseHeader [] = []
 parseHeader ('[' : t) = parseHeader (reverse t)
 parseHeader (']' : t) = trim (reverse t)
+
+----------- Main function ------------
 
 parseDconf :: IO ()
 parseDconf = do
@@ -63,3 +85,10 @@ parseDconf = do
           Nothing -> ""
           Just e  -> entryToNix e
     in  ys ++ (iter $ drop (length e + 1) xs)
+
+------------ Utils -----------------
+
+trim :: String -> String
+trim xs =
+  let trim' ys = dropWhile (== ' ') ys
+  in  trim' (reverse $ trim' (reverse xs))
