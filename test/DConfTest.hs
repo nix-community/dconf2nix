cd0618a46d3581a9968f5f6df7d30a4315e9d8ed
Gabriel Volpe <volpegabriel@gmail.com>
2020-08-08 00:15:30 +0200

    dconf2nix end to end tests

--- test/DConfTest.hs
+++ test/DConfTest.hs
@@ -7,7 +7,6 @@ where
 
 import qualified Data.Map                      as M
 import qualified Data.Text                     as T
-import qualified Data.Text.IO                  as T
 import           Data.Text                      ( Text )
 import           DConf                          ( dconfParser )
 import           DConf.Data
@@ -17,19 +16,10 @@ import           Text.Parsec                    ( runParser )
 prop_simple_parser :: Property
 prop_simple_parser = withTests (100 :: TestLimit) simpleParser
 
-prop_file_parser :: Property
-prop_file_parser = withTests (10 :: TestLimit) fileParser
-
 simpleParser :: Property
 simpleParser =
   let entries = runParser (dconfParser Normal) () "<test>" testInput
   in  property $ entries === Right [testOutput]
-
-fileParser :: Property
-fileParser = property $ do
-  input   <- evalIO $ T.readFile "data/dconf.settings"
-  entries <- evalEither $ runParser (dconfParser Normal) () "<test>" input
-  length entries === 64
 
 testInput :: Text
 testInput = T.unlines
