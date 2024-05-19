be9ec51232960556f42e60272afc3118fef27ab5
Jan Tojnar <jtojnar@gmail.com>
2024-05-19 20:57:38 +0200

    tests: Build properties dynamically
    Instead of using fixed functions.
    This will allow us to easily add NixOS variants of the tests.

--- test/DConf2NixTest.hs
+++ test/DConf2NixTest.hs
@@ -6,6 +6,7 @@ module DConf2NixTest
 where
 
 import           Data.IORef
+import           Data.String                    ( fromString )
 import qualified Data.Text                     as T
 import qualified Data.Text.IO                  as T
 import           DConf                          ( dconfParser )
@@ -13,6 +14,39 @@ import           DConf2Nix                      ( handler )
 import           DConf.Data
 import           Hedgehog
 import           Text.Parsec                    ( runParser )
+
+dconf2nixTests :: Group
+dconf2nixTests = Group "Tests"
+  [(fromString name, runOnce (inputTestProperty (fromString name) root)) | InputTest name root <- inputTests]
+
+inputTests :: [InputTest]
+inputTests =
+  [ it "bytestring"
+  , (it "custom") { itRoot = Root "ca/desrt/dconf-editor" }
+  , it "dict"
+  , it "dump"
+  , it "clocks"
+  , it "emoji"
+  , it "headers"
+  , it "indexer"
+  , it "json"
+  , it "keybindings"
+  , (it "nested") { itRoot = Root "org/gnome/desktop/peripherals" }
+  , it "numbers"
+  , it "scientific-notation"
+  , it "tuples"
+  , it "typed"
+  , it "unicode"
+  , it "variant"
+  ]
+
+data InputTest = InputTest
+  { itName :: String
+  , itRoot :: Root
+  }
+
+it :: String -> InputTest
+it name = InputTest { itName = name, itRoot = (Root T.empty) }
 
 -- | No need to repeat tests that do not use generator.
 runOnce :: Property -> Property
@@ -30,142 +64,8 @@ baseProperty i o root = property $ do
   entries = runParser (dconfParser Normal) () "<test>"
   writer ref x = modifyIORef ref (`T.append` x)
 
-dump :: Property
-dump =
-  let input  = "data/dconf.settings"
-      output = "output/dconf.nix"
-      root   = Root T.empty
+inputTestProperty :: FilePath -> Root -> Property
+inputTestProperty name root =
+  let input  = "data/" <> name <> ".settings"
+      output = "output/" <> name <> ".nix"
   in  baseProperty input output root
-
-bytestring :: Property
-bytestring =
-  let input  = "data/bytestring.settings"
-      output = "output/bytestring.nix"
-      root   = Root T.empty
-  in  baseProperty input output root
-
-customRoot :: Property
-customRoot =
-  let input  = "data/custom.settings"
-      output = "output/custom.nix"
-      root   = Root "ca/desrt/dconf-editor"
-  in  baseProperty input output root
-
-customNestedRoot :: Property
-customNestedRoot =
-  let input  = "data/nested.settings"
-      output = "output/nested.nix"
-      root   = Root "org/gnome/desktop/peripherals"
-  in  baseProperty input output root
-
-dict :: Property
-dict =
-  let input  = "data/dict.settings"
-      output = "output/dict.nix"
-      root   = Root T.empty
-  in  baseProperty input output root
-
-indexer :: Property
-indexer =
-  let input  = "data/indexer.settings"
-      output = "output/indexer.nix"
-      root   = Root T.empty
-  in  baseProperty input output root
-
-numbers :: Property
-numbers =
-  let input  = "data/numbers.settings"
-      output = "output/numbers.nix"
-      root   = Root T.empty
-  in  baseProperty input output root
-
-json :: Property
-json =
-  let input  = "data/json.settings"
-      output = "output/json.nix"
-      root   = Root T.empty
-  in  baseProperty input output root
-
-clocks :: Property
-clocks =
-  let input  = "data/clocks.settings"
-      output = "output/clocks.nix"
-      root   = Root T.empty
-  in  baseProperty input output root
-
-keybindings :: Property
-keybindings =
-  let input  = "data/keybindings.settings"
-      output = "output/keybindings.nix"
-      root   = Root T.empty
-  in  baseProperty input output root
-
-scientificNotation :: Property
-scientificNotation =
-  let input  = "data/scientific-notation.settings"
-      output = "output/scientific-notation.nix"
-      root   = Root T.empty
-  in  baseProperty input output root
-
-headers :: Property
-headers =
-  let input  = "data/headers.settings"
-      output = "output/headers.nix"
-      root   = Root T.empty
-  in  baseProperty input output root
-
-tuples :: Property
-tuples =
-  let input  = "data/tuples.settings"
-      output = "output/tuples.nix"
-      root   = Root T.empty
-  in  baseProperty input output root
-
-typed :: Property
-typed =
-  let input  = "data/typed.settings"
-      output = "output/typed.nix"
-      root   = Root T.empty
-  in  baseProperty input output root
-
-unicode :: Property
-unicode =
-  let input  = "data/unicode.settings"
-      output = "output/unicode.nix"
-      root   = Root T.empty
-  in  baseProperty input output root
-
-variant :: Property
-variant =
-  let input  = "data/variant.settings"
-      output = "output/variant.nix"
-      root   = Root T.empty
-  in  baseProperty input output root
-
-emoji :: Property
-emoji =
-  let input  = "data/emoji.settings"
-      output = "output/emoji.nix"
-      root   = Root T.empty
-  in  baseProperty input output root
-
-dconf2nixTests :: Group
-dconf2nixTests = Group "Tests"
-  [ ("bytestring", runOnce bytestring)
-  , ("clocks", runOnce clocks)
-  , ("customNestedRoot", runOnce customNestedRoot)
-  , ("customRoot", runOnce customRoot)
-  , ("dict", runOnce dict)
-  , ("dump", runOnce dump)
-  , ("emoji", runOnce emoji)
-  , ("headers", runOnce headers)
-  , ("indexer", runOnce indexer)
-  , ("json", runOnce json)
-  , ("keybindings", runOnce keybindings)
-  , ("numbers", runOnce numbers)
-  , ("scientificNotation", runOnce scientificNotation)
-  , ("tuples", runOnce tuples)
-  , ("typed", runOnce typed)
-  , ("unicode", runOnce unicode)
-  , ("variant", runOnce variant)
-  ]
