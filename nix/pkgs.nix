564bbca94084da71be5e08216a4b8b2b260ed6dd
Jan Tojnar <jtojnar@gmail.com>
2024-05-20 00:58:54 +0200

    chore: Use npins for fetching Nixpkgs
    This will make updates much more convenient.

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -3,12 +3,8 @@
 }:
 
 let
-  nixpkgs = builtins.fetchTarball {
-    name = "nixos-unstable-2024-04-11";
-    url = "https://github.com/NixOS/nixpkgs/archive/1042fd8b148a9105f3c0aca3a6177fd1d9360ba5.tar.gz";
-    sha256 = "0b2pcygqbx6m961mly2rfzp8xx52iik6silng85np9cvb4xxdiny";
-  };
-  pkgs = import nixpkgs { };
+  sources = import ../npins;
+  pkgs = import sources.nixpkgs { };
 
   hp = pkgs.haskell.packages.${compiler}.override {
     overrides = newPkgs: oldPkgs: { };
